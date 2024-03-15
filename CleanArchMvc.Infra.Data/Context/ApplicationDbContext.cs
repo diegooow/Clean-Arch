bc063266d8fdf24c44f9c95473869e84ff07582c
diego <diegomachadocordeiro@gmail.com>
2024-03-15 20:50:32 -0300

    feat: :sparkles: Add new Entities configurations
    Add new Entities configurations about the classes Product and Categories

--- CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
+++ CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
@@ -1,13 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CleanArchMvc.Domain.Entity;
 using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchMvc.Infra.Data.Context
 {
     public class ApplicationDbContext : DbContext
     {
-        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+        DbSet<Category> Categories {get;set;}
+        DbSet<Product> Products {get;set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
     }
 }
