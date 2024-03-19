be6c4c62383905c9f0ec056fc77010e4da59565b
diego <diegomachadocordeiro@gmail.com>
2024-03-19 14:33:36 -0300

    add new migrations for user login/security

--- CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
+++ CleanArchMvc.Infra.Data/Context/ApplicationDbContext.cs
@@ -1,16 +1,18 @@
 using CleanArchMvc.Domain.Entity;
+using CleanArchMvc.Infra.Data.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchMvc.Infra.Data.Context
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            
+
         }
-        public DbSet<Category> Categories {get;set;}
-        public DbSet<Product> Products {get;set;}
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
