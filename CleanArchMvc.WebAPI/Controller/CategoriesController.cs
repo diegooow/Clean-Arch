2fd5fb71bd1219f599d482cb8ed91a23360d7423
diego <diegomachadocordeiro@gmail.com>
2024-03-22 10:45:52 -0300

    feat: :sparkles: Add product controller

--- CleanArchMvc.WebAPI/Controller/CategoriesController.cs
+++ CleanArchMvc.WebAPI/Controller/CategoriesController.cs
@@ -1,51 +1,82 @@
-using CleanArchMvc.Application.DTOs;
+﻿using CleanArchMvc.Application.DTOs;
 using CleanArchMvc.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-namespace CleanArchMvc.WebAPI.Controller;
-
-[ApiController]
-[Route("api/[controller]")]
-public class CategoriesController : ControllerBase
+namespace CleanArchMvc.API.Controllers
 {
-    private readonly ICategoryService _categoryService;
-    public CategoriesController(ICategoryService categoryService)
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
     {
-        _categoryService = categoryService;
-    }
-    
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get()
-    {
-        var categories = await _categoryService.GetCategories();
-        if(categories == null){
-            return NotFound("Categories not found");
-
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
         }
 
-        return Ok(categories);
-    }
-
-    [HttpGet("{id:int}", Name ="GetCategory")]
-    public async Task<ActionResult<IEnumerable<CategoryDTO>>> GetById(int id)
-    {
-        var category = await _categoryService.GetById(id);
-        if(category == null)
-            return NotFound("Category not found");
-
-        return Ok(category);
-    }
-
-    [HttpPost]
-    public async Task<ActionResult<IEnumerable<CategoryDTO>>> Create([FromBody] CategoryDTO categoryDTO)
-    {
-        if(categoryDTO ==null){
-            return BadRequest("Invalid Data");
-        }else{
-
-        await _categoryService.Add(categoryDTO);      
-        return new CreatedAtRouteResult("GetCategory",new {id = categoryDTO.Id},categoryDTO);
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> Get()
+        {
+            var categories = await _categoryService.GetCategories();
+            if (categories == null)
+            {
+                return NotFound("Categories not found");
+            }
+            return Ok(categories);
         }
 
+        [HttpGet("{id:int}", Name = "GetCategory")]
+        public async Task<ActionResult<CategoryDTO>> Get(int id)
+        {
+            var category = await _categoryService.GetById(id);
+            if (category == null)
+            {
+                return NotFound("Category not found");
+            }
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CategoryDTO categoryDto)
+        {
+            if (categoryDto == null)
+                return BadRequest("Invalid Data");
+
+            await _categoryService.Add(categoryDto);
+
+            return new CreatedAtRouteResult("GetCategory", new { id = categoryDto.Id }, 
+                categoryDto);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put(int id,[FromBody] CategoryDTO categoryDto)
+        {
+            if (id != categoryDto.Id)
+                return BadRequest();
+
+            if (categoryDto == null)
+                return BadRequest();
+
+            await _categoryService.Update(categoryDto);
+
+            return Ok(categoryDto);
+        }          
+        
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<CategoryDTO>> Delete(int id)
+        {
+            var category = await _categoryService.GetById(id);
+            if(category == null)
+            {
+                return NotFound("Category not found");
+            }
+            
+            await _categoryService.Delete(id);
+
+            return Ok(category);
+
+        }
     }
 }
